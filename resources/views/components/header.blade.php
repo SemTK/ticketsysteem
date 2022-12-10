78e7a7be351ed482fc668ca5ee581e0930b62210
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-10 20:35:02 +0100

    adjusted header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,25 +10,21 @@
                     <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about-us') }}">Over Ons</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="{{ route('events') }}">List of Events</a>
                 </li>
                 @if(Auth::user())
-                <a class="nav-link" href="{{ route('add-event') }}">Add event</a>
+                <a class="nav-link" href="">Mijn tickets</a>
                 @endif
             </ul>
             
             <span class="navbar-text">
                 @if(Auth::user())
+
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
                     <button type="submit" class="nav-link">logout</button>
                 </form>
+                
                 @else
                 <a href=" {{ route('register') }} ">Registreren</a>
                 <a href=" {{ route('login') }}">Inloggen</a>
