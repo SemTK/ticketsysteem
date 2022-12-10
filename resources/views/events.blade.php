8d6c9d302574f563aba809856e51e0f5f3b58231
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-10 20:35:39 +0100

    added correct permissions to event website

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -4,6 +4,13 @@
 
 <h1>List of all current and future Events</h1>
 
+<div class="add-event-button">
+    @if(Auth::user() && Auth::user()->isAdmin == 1)
+    <a class="btn btn-primary" href=" {{ route('add-event') }} ">Create Event</a>
+    @endif
+</div>
+
+
 <div class="events">
     @foreach($events as $event)
     <div class="card" style="width: 18rem;">
@@ -12,11 +19,29 @@
             <h5 class="card-title">{{ $event->name }}</h5>
             <p class="card-text">{{ $event->description }}</p>
         </div>
+        @if(Auth::user())
         <ul class="list-group list-group-flush">
             <li class="list-group-item">Location: {{ $event->location }}</li>
             <li class="list-group-item">Price: ${{ $event->price }}</li>
             <li class="list-group-item">Amount of Tickets left: {{ $event->available_tickets }}</li>
+            @if(Auth::user() && Auth::user()->isAdmin == 1)
+
+            <li class="list-group-item">
+                <div class="event-edit-buttons">
+                    
+                    <a href="{{ route('viewEditPage', $event->id)}}" class="btn btn-primary">Edit Event</a>
+
+                    <form action="{{ route('deleteEvent', $event->id) }}" method="POST">
+                        @csrf
+                        <button class="btn btn-danger" type="submit" onclick="return confirm('Are you sure you want to delete this event?')">Delete Event</button>
+                    </form>
+
+                </div>
+            </li>
+
+            @endif
         </ul>
+        @endif
     </div>
     @endforeach
 </div>
