00ed6e3983874d59c575450653cc55fe50eaa7bf
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-11 17:10:47 +0100

    sorted routes

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,27 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\TicketsController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 // ->middleware('auth')
 
+// GET Routes
 Route::get('/', [PagesController::class, 'viewHome'])->name('home');
 
-Route::post('/save-ticket', [TicketsController::class, 'saveTicket'])->name('saveTicket');
+Route::get('/my-tickets', [PagesController::class, 'viewMyTicketsPage'])->name('my-tickets')->middleware('auth');
 
-Route::post('/add-event', [EventController::class, 'addEvent'])->name('create-event');
+Route::get('/edit-event/{eventId}', [PagesController::class, 'viewEditPage'])->name('viewEditPage')->middleware('admin');
 
 Route::get('/add-event', [PagesController::class, 'viewAddEvent'])->name('add-event')->middleware('auth');
 
 Route::get('/events', [PagesController::class, 'viewEvents'])->name('events');
 
+// POST Routes
 Route::post('/event/deleted/{eventId}', [EventController::class, 'deleteEvent'])->name('deleteEvent')->middleware('admin');
 
-Route::get('/edit-event/{eventId}', [PagesController::class, 'viewEditPage'])->name('viewEditPage')->middleware('admin');
-
 Route::post('/event-edited/{eventId}', [EventController::class, 'editEvent'])->name('editEvent')->middleware('admin');
+
+Route::post('/add-event', [EventController::class, 'addEvent'])->name('create-event')->middleware('admin');
 
 require __DIR__.'/auth.php';
